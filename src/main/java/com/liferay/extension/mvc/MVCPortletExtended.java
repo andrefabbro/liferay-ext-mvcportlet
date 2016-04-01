0cac80522469fc97bac05c1641e151e165c4a77e
André Fabbro <andre.fabbro@liferay.com>
2016-04-01 16:30:29 -0500

    Changing the code style format

--- src/main/java/com/liferay/extension/mvc/MVCPortletExtended.java
+++ src/main/java/com/liferay/extension/mvc/MVCPortletExtended.java
@@ -46,271 +46,274 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
  */
 public class MVCPortletExtended extends MVCPortlet {
 
-    protected static final ObjectMapper JSON_MAPPER = new ObjectMapper();
-    private final Map<String, Method> _resourceMethods = new ConcurrentHashMap<>();
+	protected static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+	private final Map<String, Method> _resourceMethods = new ConcurrentHashMap<>();
 
-    @Override
-    public void serveResource(ResourceRequest resourceRequest,
-	    ResourceResponse resourceResponse)
-	    throws IOException, PortletException {
+	@Override
+	public void serveResource(ResourceRequest resourceRequest,
+			ResourceResponse resourceResponse) throws IOException,
+			PortletException {
 
-	invokeHideDefaultSuccessMessage(resourceRequest);
+		invokeHideDefaultSuccessMessage(resourceRequest);
 
-	String path = getPath(resourceRequest, resourceResponse);
+		String path = getPath(resourceRequest, resourceResponse);
 
-	if (path != null) {
-	    include(path, resourceRequest, resourceResponse,
-		    PortletRequest.RESOURCE_PHASE);
-	}
-
-	invokeResourceExtStack(resourceRequest, resourceResponse);
-    }
-
-    protected void invokeResourceExtStack(ResourceRequest resourceRequest,
-	    ResourceResponse resourceResponse)
-	    throws IOException, PortletException {
-
-	if (!isProcessResourceRequest(resourceRequest)) {
-	    return;
-	}
-
-	if (!callResourceMethod(resourceRequest, resourceResponse)) {
-	    return;
-	}
-
-	if (!SessionErrors.isEmpty(resourceRequest)) {
-	    return;
-	}
-
-	if (!SessionMessages.isEmpty(resourceRequest)) {
-	    return;
-	}
-
-	// code from default GenericPortlet Impl
-	if (resourceRequest.getResourceID() != null) {
-	    PortletRequestDispatcher rd = getPortletConfig().getPortletContext()
-		    .getRequestDispatcher(resourceRequest.getResourceID());
-	    if (rd != null)
-		rd.forward(resourceRequest, resourceResponse);
-	}
-    }
-
-    protected void invokeHideDefaultSuccessMessage(
-	    PortletRequest portletRequest) {
-
-	boolean hideDefaultSuccessMessage = ParamUtil.getBoolean(portletRequest,
-		"hideDefaultSuccessMessage");
-
-	if (hideDefaultSuccessMessage) {
-	    hideDefaultSuccessMessage(portletRequest);
-	}
-    }
-
-    protected void hideDefaultSuccessMessage(PortletRequest portletRequest) {
-
-	SessionMessages.add(portletRequest, PortalUtil
-		.getPortletId(portletRequest)
-		+ SessionMessages.KEY_SUFFIX_HIDE_DEFAULT_SUCCESS_MESSAGE);
-    }
-
-    protected String getPath(PortletRequest portletRequest,
-	    PortletResponse portletResponse) {
-
-	String mvcPath = portletRequest.getParameter("mvcPath");
-
-	if (mvcPath == null) {
-	    mvcPath = (String) portletRequest.getAttribute(
-		    getMVCPathAttributeName(portletResponse.getNamespace()));
-	}
-
-	// Check deprecated parameter
-
-	if (mvcPath == null) {
-	    mvcPath = portletRequest.getParameter("jspPage");
-	}
-
-	return mvcPath;
-    }
-
-    protected String getMVCPathAttributeName(String namespace) {
-
-	return namespace.concat(StringPool.PERIOD)
-		.concat(MVCRenderConstantsExt.MVC_PATH_REQUEST_ATTRIBUTE_NAME);
-    }
-
-    protected boolean callResourceMethod(ResourceRequest resourceRequest,
-	    ResourceResponse resourceResponse) throws PortletException {
-
-	String actionName = ParamUtil.getString(resourceRequest,
-		ActionRequest.ACTION_NAME);
-
-	if (Validator.isNull(actionName)
-		|| actionName.equals("callResourceMethod")
-		|| actionName.equals("serveResource")) {
-
-	    return false;
-	}
-
-	try {
-	    Method method = getResourceMethod(actionName);
-
-	    checkPermissions(resourceRequest, method);
-
-	    invokeMethod(resourceRequest, resourceResponse, method);
-
-	    return true;
-	} catch (NoSuchMethodException nsme) {
-	    try {
-		super.serveResource(resourceRequest, resourceResponse);
-
-		return true;
-	    } catch (Exception e) {
-		throw new PortletException(e);
-	    }
-	} catch (InvocationTargetException ite) {
-	    Throwable cause = ite.getCause();
-
-	    if (cause != null) {
-		throw new PortletException(cause);
-	    } else {
-		throw new PortletException(ite);
-	    }
-	} catch (Exception e) {
-	    throw new PortletException(e);
-	}
-    }
-
-    /**
-     * Check the permissions based on the annotation
-     * {@link com.liferay.extension.mvc.security.PortletSecured PortletSecured}
-     * 
-     * @param portletRequest
-     * @param method
-     * @throws PrincipalException
-     */
-    protected void checkPermissions(PortletRequest portletRequest,
-	    Method method) throws PrincipalException {
-
-	if (!method.isAnnotationPresent(PortletSecured.class)) {
-	    return;
-	}
-
-	PortletSecured securedAnnotation = method
-		.getAnnotation(PortletSecured.class);
-
-	String[] permissions = securedAnnotation.value();
-
-	ThemeDisplay themeDisplay = (ThemeDisplay) portletRequest
-		.getAttribute(WebKeys.THEME_DISPLAY);
-
-	PermissionChecker permissionChecker = themeDisplay
-		.getPermissionChecker();
-
-	if (permissionChecker.isOmniadmin())
-	    return;
-
-	long groupId = themeDisplay.getScopeGroupId();
-	String name = themeDisplay.getPortletDisplay().getPortletName();
-	String primKey = themeDisplay.getPortletDisplay().getResourcePK();
-
-	if (primKey == null || primKey.isEmpty())
-	    primKey = PortletPermissionUtil
-		    .getPrimaryKey(themeDisplay.getLayout().getPlid(), name);
-
-	for (String permission : permissions) {
-	    if (!permissionChecker.hasPermission(groupId, name, primKey,
-		    permission))
-		throw new PrincipalException();
-	}
-    }
-
-    private void invokeMethod(ResourceRequest resourceRequest,
-	    ResourceResponse resourceResponse, Method method)
-	    throws IllegalAccessException, InvocationTargetException,
-	    IOException {
-
-	if (!method.isAnnotationPresent(PortletJSONResource.class)) {
-	    method.invoke(this, resourceRequest, resourceResponse);
-	    return;
-	}
-
-	PortletJSONResource annotation = method
-		.getAnnotation(PortletJSONResource.class);
-	Class attributeClass = annotation.attributeClass();
-
-	Object invoke;
-	if (attributeClass != void.class) {
-	    invoke = method.invoke(this, resourceRequest, resourceResponse,
-		    readObjectFromBody(resourceRequest, attributeClass));
-	} else {
-	    invoke = method.invoke(this, resourceRequest, resourceResponse);
-	}
-
-	if (annotation.jsonResponse()) {
-	    jsonResponse(resourceResponse, invoke);
-	}
-    }
-
-    protected Method getResourceMethod(String actionName)
-	    throws NoSuchMethodException {
-
-	Method result = _resourceMethods.get(actionName);
-
-	if (result != null) {
-	    return result;
-	}
-
-	Class<?> clazz = getClass();
-
-	result = getMethodByActionName(actionName, clazz);
-
-	_resourceMethods.put(actionName, result);
-
-	return result;
-    }
-
-    private Method getMethodByActionName(String actionName, Class<?> clazz)
-	    throws NoSuchMethodException {
-
-	try {
-	    return clazz.getMethod(actionName, ResourceRequest.class,
-		    ResourceResponse.class);
-	} catch (NoSuchMethodException e) {
-	    Method[] methods = clazz.getMethods();
-	    for (Method method : methods) {
-		if (!method.getName().equals(actionName) || !method
-			.isAnnotationPresent(PortletJSONResource.class)) {
-		    continue;
+		if (path != null) {
+			include(path, resourceRequest, resourceResponse,
+					PortletRequest.RESOURCE_PHASE);
 		}
+
+		invokeResourceExtStack(resourceRequest, resourceResponse);
+	}
+
+	protected void invokeResourceExtStack(ResourceRequest resourceRequest,
+			ResourceResponse resourceResponse) throws IOException,
+			PortletException {
+
+		if (!isProcessResourceRequest(resourceRequest)) {
+			return;
+		}
+
+		if (!callResourceMethod(resourceRequest, resourceResponse)) {
+			return;
+		}
+
+		if (!SessionErrors.isEmpty(resourceRequest)) {
+			return;
+		}
+
+		if (!SessionMessages.isEmpty(resourceRequest)) {
+			return;
+		}
+
+		// code from default GenericPortlet Impl
+		if (resourceRequest.getResourceID() != null) {
+			PortletRequestDispatcher rd = getPortletConfig()
+					.getPortletContext().getRequestDispatcher(
+							resourceRequest.getResourceID());
+			if (rd != null)
+				rd.forward(resourceRequest, resourceResponse);
+		}
+	}
+
+	protected void invokeHideDefaultSuccessMessage(PortletRequest portletRequest) {
+
+		boolean hideDefaultSuccessMessage = ParamUtil.getBoolean(
+				portletRequest, "hideDefaultSuccessMessage");
+
+		if (hideDefaultSuccessMessage) {
+			hideDefaultSuccessMessage(portletRequest);
+		}
+	}
+
+	protected void hideDefaultSuccessMessage(PortletRequest portletRequest) {
+
+		SessionMessages
+				.add(portletRequest,
+						PortalUtil.getPortletId(portletRequest)
+								+ SessionMessages.KEY_SUFFIX_HIDE_DEFAULT_SUCCESS_MESSAGE);
+	}
+
+	protected String getPath(PortletRequest portletRequest,
+			PortletResponse portletResponse) {
+
+		String mvcPath = portletRequest.getParameter("mvcPath");
+
+		if (mvcPath == null) {
+			mvcPath = (String) portletRequest
+					.getAttribute(getMVCPathAttributeName(portletResponse
+							.getNamespace()));
+		}
+
+		// Check deprecated parameter
+
+		if (mvcPath == null) {
+			mvcPath = portletRequest.getParameter("jspPage");
+		}
+
+		return mvcPath;
+	}
+
+	protected String getMVCPathAttributeName(String namespace) {
+
+		return namespace.concat(StringPool.PERIOD).concat(
+				MVCRenderConstantsExt.MVC_PATH_REQUEST_ATTRIBUTE_NAME);
+	}
+
+	protected boolean callResourceMethod(ResourceRequest resourceRequest,
+			ResourceResponse resourceResponse) throws PortletException {
+
+		String actionName = ParamUtil.getString(resourceRequest,
+				ActionRequest.ACTION_NAME);
+
+		if (Validator.isNull(actionName)
+				|| actionName.equals("callResourceMethod")
+				|| actionName.equals("serveResource")) {
+
+			return false;
+		}
+
+		try {
+			Method method = getResourceMethod(actionName);
+
+			checkPermissions(resourceRequest, method);
+
+			invokeMethod(resourceRequest, resourceResponse, method);
+
+			return true;
+		} catch (NoSuchMethodException nsme) {
+			try {
+				super.serveResource(resourceRequest, resourceResponse);
+
+				return true;
+			} catch (Exception e) {
+				throw new PortletException(e);
+			}
+		} catch (InvocationTargetException ite) {
+			Throwable cause = ite.getCause();
+
+			if (cause != null) {
+				throw new PortletException(cause);
+			} else {
+				throw new PortletException(ite);
+			}
+		} catch (Exception e) {
+			throw new PortletException(e);
+		}
+	}
+
+	/**
+	 * Check the permissions based on the annotation
+	 * {@link com.liferay.extension.mvc.security.PortletSecured PortletSecured}
+	 * 
+	 * @param portletRequest
+	 * @param method
+	 * @throws PrincipalException
+	 */
+	protected void checkPermissions(PortletRequest portletRequest, Method method)
+			throws PrincipalException {
+
+		if (!method.isAnnotationPresent(PortletSecured.class)) {
+			return;
+		}
+
+		PortletSecured securedAnnotation = method
+				.getAnnotation(PortletSecured.class);
+
+		String[] permissions = securedAnnotation.value();
+
+		ThemeDisplay themeDisplay = (ThemeDisplay) portletRequest
+				.getAttribute(WebKeys.THEME_DISPLAY);
+
+		PermissionChecker permissionChecker = themeDisplay
+				.getPermissionChecker();
+
+		if (permissionChecker.isOmniadmin())
+			return;
+
+		long groupId = themeDisplay.getScopeGroupId();
+		String name = themeDisplay.getPortletDisplay().getPortletName();
+		String primKey = themeDisplay.getPortletDisplay().getResourcePK();
+
+		if (primKey == null || primKey.isEmpty())
+			primKey = PortletPermissionUtil.getPrimaryKey(themeDisplay
+					.getLayout().getPlid(), name);
+
+		for (String permission : permissions) {
+			if (!permissionChecker.hasPermission(groupId, name, primKey,
+					permission))
+				throw new PrincipalException();
+		}
+	}
+
+	private void invokeMethod(ResourceRequest resourceRequest,
+			ResourceResponse resourceResponse, Method method)
+			throws IllegalAccessException, InvocationTargetException,
+			IOException {
+
+		if (!method.isAnnotationPresent(PortletJSONResource.class)) {
+			method.invoke(this, resourceRequest, resourceResponse);
+			return;
+		}
+
 		PortletJSONResource annotation = method
-			.getAnnotation(PortletJSONResource.class);
-		return clazz.getMethod(actionName, ResourceRequest.class,
-			ResourceResponse.class, annotation.attributeClass());
-	    }
-	    throw e;
+				.getAnnotation(PortletJSONResource.class);
+		Class attributeClass = annotation.attributeClass();
+
+		Object invoke;
+		if (attributeClass != void.class) {
+			invoke = method.invoke(this, resourceRequest, resourceResponse,
+					readObjectFromBody(resourceRequest, attributeClass));
+		} else {
+			invoke = method.invoke(this, resourceRequest, resourceResponse);
+		}
+
+		if (annotation.jsonResponse()) {
+			jsonResponse(resourceResponse, invoke);
+		}
 	}
-    }
 
-    private <T> T readObjectFromBody(ResourceRequest request, Class<T> clazz) {
+	protected Method getResourceMethod(String actionName)
+			throws NoSuchMethodException {
 
-	StringBuilder buffer = new StringBuilder();
-	try (BufferedReader reader = request.getReader()) {
-	    String line;
-	    while ((line = reader.readLine()) != null) {
-		buffer.append(line);
-	    }
-	} catch (IOException e) {
-	    return null;
+		Method result = _resourceMethods.get(actionName);
+
+		if (result != null) {
+			return result;
+		}
+
+		Class<?> clazz = getClass();
+
+		result = getMethodByActionName(actionName, clazz);
+
+		_resourceMethods.put(actionName, result);
+
+		return result;
 	}
-	return new Gson().fromJson(buffer.toString(), clazz);
-    }
 
-    private void jsonResponse(ResourceResponse response, Object object)
-	    throws IOException {
+	private Method getMethodByActionName(String actionName, Class<?> clazz)
+			throws NoSuchMethodException {
 
-	response.setContentType("application/json");
-	response.setCharacterEncoding("UTF-8");
-	JSON_MAPPER.writeValue(response.getPortletOutputStream(), object);
-    }
+		try {
+			return clazz.getMethod(actionName, ResourceRequest.class,
+					ResourceResponse.class);
+		} catch (NoSuchMethodException e) {
+			Method[] methods = clazz.getMethods();
+			for (Method method : methods) {
+				if (!method.getName().equals(actionName)
+						|| !method
+								.isAnnotationPresent(PortletJSONResource.class)) {
+					continue;
+				}
+				PortletJSONResource annotation = method
+						.getAnnotation(PortletJSONResource.class);
+				return clazz.getMethod(actionName, ResourceRequest.class,
+						ResourceResponse.class, annotation.attributeClass());
+			}
+			throw e;
+		}
+	}
+
+	private <T> T readObjectFromBody(ResourceRequest request, Class<T> clazz) {
+
+		StringBuilder buffer = new StringBuilder();
+		try (BufferedReader reader = request.getReader()) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				buffer.append(line);
+			}
+		} catch (IOException e) {
+			return null;
+		}
+		return new Gson().fromJson(buffer.toString(), clazz);
+	}
+
+	private void jsonResponse(ResourceResponse response, Object object)
+			throws IOException {
+
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		JSON_MAPPER.writeValue(response.getPortletOutputStream(), object);
+	}
 
 }
--- src/test/java/com/liferay/extension/mvc/MVCPortletExtendedTest.java
+++ src/test/java/com/liferay/extension/mvc/MVCPortletExtendedTest.java
@@ -38,170 +38,175 @@ import com.liferay.portal.theme.ThemeDisplay;
 @RunWith(MockitoJUnitRunner.class)
 public class MVCPortletExtendedTest {
 
-    private static final String PORTLET_PK = "00001";
+	private static final String PORTLET_PK = "00001";
 
-    private static final String PORTLET_NAME = "myCoolPortlet";
+	private static final String PORTLET_NAME = "myCoolPortlet";
 
-    @Mock
-    private ResourceRequest resourceRequest;
+	@Mock
+	private ResourceRequest resourceRequest;
 
-    @Mock
-    private ResourceResponse resourceResponse;
+	@Mock
+	private ResourceResponse resourceResponse;
 
-    @Mock
-    private ThemeDisplay themeDisplay;
+	@Mock
+	private ThemeDisplay themeDisplay;
 
-    @Mock
-    private PermissionChecker permissionChecker;
+	@Mock
+	private PermissionChecker permissionChecker;
 
-    @Mock
-    private PortletDisplay portletDisplay;
+	@Mock
+	private PortletDisplay portletDisplay;
 
-    @InjectMocks
-    private MyCoolPortlet myCoolPortlet = new MyCoolPortlet();
+	@InjectMocks
+	private MyCoolPortlet myCoolPortlet = new MyCoolPortlet();
 
-    private BufferedReader reader;
+	private BufferedReader reader;
 
-    private PipedInputStream pipeInput;
+	private PipedInputStream pipeInput;
 
-    @Before
-    public void setUp() {
+	@Before
+	public void setUp() {
 
-	MockitoAnnotations.initMocks(this);
+		MockitoAnnotations.initMocks(this);
 
-	when(resourceResponse.getNamespace()).thenReturn(PORTLET_NAME);
-    }
-
-    @Test
-    public void invokeSecuredAnnotatedMethodSuccess() throws Exception {
-
-	when(permissionChecker.isOmniadmin()).thenReturn(false);
-	when(permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
-		"ADD_RESOURCE")).thenReturn(true);
-	when(permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
-		"EDIT_RESOURCE")).thenReturn(true);
-
-	invokeCustomResourceAnnotatedMethodAjax("ajaxSecuredAnnotatedMethod");
-    }
-
-    @Test(expected = PortletException.class)
-    public void invokeSecuredAnnotatedMethodFail() throws Exception {
-
-	when(permissionChecker.isOmniadmin()).thenReturn(false);
-	when(permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
-		"ADD_RESOURCE")).thenReturn(true);
-	when(permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
-		"EDIT_RESOURCE")).thenReturn(false);
-
-	invokeCustomResourceAnnotatedMethodAjax("ajaxSecuredAnnotatedMethod");
-    }
-
-    @Test
-    public void dontConsiderOmniUserInPermissionChecker() throws Exception {
-
-	when(permissionChecker.isOmniadmin()).thenReturn(true);
-	when(permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
-		"ADD_RESOURCE")).thenReturn(false);
-	when(permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
-		"EDIT_RESOURCE")).thenReturn(false);
-
-	invokeCustomResourceAnnotatedMethodAjax("ajaxSecuredAnnotatedMethod");
-    }
-
-    @Test
-    public void invokeCustomResourceMethodAjaxOne()
-	    throws IOException, PortletException {
-
-	invokeCustomResourceMethodAjax("ajaxMethodOne");
-    }
-
-    @Test
-    public void invokeCustomResourceMethodAjaxTwo()
-	    throws IOException, PortletException {
-
-	invokeCustomResourceMethodAjax("ajaxMethodTwo");
-    }
-
-    @Test
-    public void invokeCustomResourceAnnotatedMethodAjaxOne()
-	    throws IOException, PortletException {
-
-	invokeCustomResourceAnnotatedMethodAjax("ajaxAnnotatedMethodOne");
-    }
-
-    private void invokeCustomResourceMethodAjax(String methodName)
-	    throws IOException, PortletException {
-
-	commonLogic(methodName);
-
-	assertEquals("executed " + methodName, reader.readLine());
-    }
-
-    private void invokeCustomResourceAnnotatedMethodAjax(String methodName)
-	    throws IOException, PortletException {
-
-	prepareReaderForRequest();
-	prepareOutputStreamForResponse();
-	commonLogic(methodName);
-
-	assertEquals(DTOConverterUtil.buildCompanyJSON(), readResponse());
-    }
-
-    private void prepareWriterForResponse() throws IOException {
-
-	PipedInputStream pipeInput = new PipedInputStream();
-	reader = new BufferedReader(new InputStreamReader(pipeInput));
-	BufferedOutputStream out = new BufferedOutputStream(
-		new PipedOutputStream(pipeInput));
-
-	when(resourceResponse.getWriter()).thenReturn(new PrintWriter(out));
-    }
-
-    private void prepareReaderForRequest() throws IOException {
-
-	BufferedReader mockReader = mock(BufferedReader.class);
-	when(mockReader.readLine())
-		.thenReturn(DTOConverterUtil.buildCompanyJSON())
-		.thenReturn(null);
-	when(resourceRequest.getReader()).thenReturn(mockReader);
-
-	when(portletDisplay.getPortletName()).thenReturn(PORTLET_NAME);
-	when(portletDisplay.getResourcePK()).thenReturn(PORTLET_PK);
-
-	when(themeDisplay.getPermissionChecker()).thenReturn(permissionChecker);
-	when(themeDisplay.getScopeGroupId()).thenReturn(0l);
-	when(themeDisplay.getPortletDisplay()).thenReturn(portletDisplay);
-
-	when(resourceRequest.getAttribute(WebKeys.THEME_DISPLAY))
-		.thenReturn(themeDisplay);
-    }
-
-    private void prepareOutputStreamForResponse() throws IOException {
-
-	pipeInput = new PipedInputStream();
-	OutputStream portletOutputStream = new PipedOutputStream(pipeInput);
-	when(resourceResponse.getPortletOutputStream())
-		.thenReturn(portletOutputStream);
-    }
-
-    private void commonLogic(String methodName)
-	    throws IOException, PortletException {
-
-	prepareWriterForResponse();
-	when(resourceRequest.getParameter(ActionRequest.ACTION_NAME))
-		.thenReturn(methodName);
-	myCoolPortlet.serveResource(resourceRequest, resourceResponse);
-    }
-
-    private String readResponse() throws IOException {
-
-	String result = "";
-	byte[] buffer = new byte[1024];
-	int read;
-	while ((read = pipeInput.read(buffer)) != -1) {
-	    result = result.concat(new String(buffer, 0, read));
+		when(resourceResponse.getNamespace()).thenReturn(PORTLET_NAME);
 	}
-	return result;
-    }
+
+	@Test
+	public void invokeSecuredAnnotatedMethodSuccess() throws Exception {
+
+		when(permissionChecker.isOmniadmin()).thenReturn(false);
+		when(
+				permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
+						"ADD_RESOURCE")).thenReturn(true);
+		when(
+				permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
+						"EDIT_RESOURCE")).thenReturn(true);
+
+		invokeCustomResourceAnnotatedMethodAjax("ajaxSecuredAnnotatedMethod");
+	}
+
+	@Test(expected = PortletException.class)
+	public void invokeSecuredAnnotatedMethodFail() throws Exception {
+
+		when(permissionChecker.isOmniadmin()).thenReturn(false);
+		when(
+				permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
+						"ADD_RESOURCE")).thenReturn(true);
+		when(
+				permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
+						"EDIT_RESOURCE")).thenReturn(false);
+
+		invokeCustomResourceAnnotatedMethodAjax("ajaxSecuredAnnotatedMethod");
+	}
+
+	@Test
+	public void dontConsiderOmniUserInPermissionChecker() throws Exception {
+
+		when(permissionChecker.isOmniadmin()).thenReturn(true);
+		when(
+				permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
+						"ADD_RESOURCE")).thenReturn(false);
+		when(
+				permissionChecker.hasPermission(0, PORTLET_NAME, PORTLET_PK,
+						"EDIT_RESOURCE")).thenReturn(false);
+
+		invokeCustomResourceAnnotatedMethodAjax("ajaxSecuredAnnotatedMethod");
+	}
+
+	@Test
+	public void invokeCustomResourceMethodAjaxOne() throws IOException,
+			PortletException {
+
+		invokeCustomResourceMethodAjax("ajaxMethodOne");
+	}
+
+	@Test
+	public void invokeCustomResourceMethodAjaxTwo() throws IOException,
+			PortletException {
+
+		invokeCustomResourceMethodAjax("ajaxMethodTwo");
+	}
+
+	@Test
+	public void invokeCustomResourceAnnotatedMethodAjaxOne()
+			throws IOException, PortletException {
+
+		invokeCustomResourceAnnotatedMethodAjax("ajaxAnnotatedMethodOne");
+	}
+
+	private void invokeCustomResourceMethodAjax(String methodName)
+			throws IOException, PortletException {
+
+		commonLogic(methodName);
+
+		assertEquals("executed " + methodName, reader.readLine());
+	}
+
+	private void invokeCustomResourceAnnotatedMethodAjax(String methodName)
+			throws IOException, PortletException {
+
+		prepareReaderForRequest();
+		prepareOutputStreamForResponse();
+		commonLogic(methodName);
+
+		assertEquals(DTOConverterUtil.buildCompanyJSON(), readResponse());
+	}
+
+	private void prepareWriterForResponse() throws IOException {
+
+		PipedInputStream pipeInput = new PipedInputStream();
+		reader = new BufferedReader(new InputStreamReader(pipeInput));
+		BufferedOutputStream out = new BufferedOutputStream(
+				new PipedOutputStream(pipeInput));
+
+		when(resourceResponse.getWriter()).thenReturn(new PrintWriter(out));
+	}
+
+	private void prepareReaderForRequest() throws IOException {
+
+		BufferedReader mockReader = mock(BufferedReader.class);
+		when(mockReader.readLine()).thenReturn(
+				DTOConverterUtil.buildCompanyJSON()).thenReturn(null);
+		when(resourceRequest.getReader()).thenReturn(mockReader);
+
+		when(portletDisplay.getPortletName()).thenReturn(PORTLET_NAME);
+		when(portletDisplay.getResourcePK()).thenReturn(PORTLET_PK);
+
+		when(themeDisplay.getPermissionChecker()).thenReturn(permissionChecker);
+		when(themeDisplay.getScopeGroupId()).thenReturn(0l);
+		when(themeDisplay.getPortletDisplay()).thenReturn(portletDisplay);
+
+		when(resourceRequest.getAttribute(WebKeys.THEME_DISPLAY)).thenReturn(
+				themeDisplay);
+	}
+
+	private void prepareOutputStreamForResponse() throws IOException {
+
+		pipeInput = new PipedInputStream();
+		OutputStream portletOutputStream = new PipedOutputStream(pipeInput);
+		when(resourceResponse.getPortletOutputStream()).thenReturn(
+				portletOutputStream);
+	}
+
+	private void commonLogic(String methodName) throws IOException,
+			PortletException {
+
+		prepareWriterForResponse();
+		when(resourceRequest.getParameter(ActionRequest.ACTION_NAME))
+				.thenReturn(methodName);
+		myCoolPortlet.serveResource(resourceRequest, resourceResponse);
+	}
+
+	private String readResponse() throws IOException {
+
+		String result = "";
+		byte[] buffer = new byte[1024];
+		int read;
+		while ((read = pipeInput.read(buffer)) != -1) {
+			result = result.concat(new String(buffer, 0, read));
+		}
+		return result;
+	}
 
 }
